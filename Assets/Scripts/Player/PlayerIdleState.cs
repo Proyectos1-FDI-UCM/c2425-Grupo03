586895fce0a53ead0f00ca745209a34b8ca4f2ba
LentaAlexandra <66795326+LentaAlexandra@users.noreply.github.com>
2025-02-22 19:27:15 +0100

    Movimiento Lateral implementado
    + cambio de nombre de escena

---
+++ Assets/Scripts/Player/PlayerIdleState.cs
@@ -0,0 +1,106 @@
+//---------------------------------------------------------
+// Estado inactivo del jugador.
+// Alexandra Lenta
+// Kingless Dungeon
+// Proyectos 1 - Curso 2024-25
+//---------------------------------------------------------
+
+using UnityEngine;
+
+/// <summary>
+/// Antes de cada class, descripción de qué es y para qué sirve,
+/// usando todas las líneas que sean necesarias.
+/// </summary>
+public class PlayerIdleState : BaseState
+{
+    // ---- ATRIBUTOS DEL INSPECTOR ----
+    #region Atributos del Inspector (serialized fields)
+    // Documentar cada atributo que aparece aquí.
+    // Puesto que son atributos globales en la clase debes usar "_" + camelCase para su nombre.
+
+    #endregion
+
+    // ---- ATRIBUTOS PRIVADOS ----
+    #region Atributos Privados (private fields)
+    /// <summary>
+    /// El valor del input del jugador.
+    /// Left = -1, Right = 1, No Input = 0.
+    /// </summary>
+    private float _moveDir;
+    #endregion
+
+    // ---- PROPIEDADES ----
+    #region Propiedades
+    // Documentar cada propiedad que aparece aquí.
+    // Escribir con PascalCase.
+    #endregion
+
+    // ---- MÉTODOS DE MONOBEHAVIOUR ----
+    #region Métodos de MonoBehaviour
+
+    #endregion
+
+    // ---- MÉTODOS PÚBLICOS ----
+    #region Métodos públicos
+    // Documentar cada método que aparece aquí con ///<summary>
+    // El convenio de nombres de Unity recomienda que estos métodos
+    // se nombren en formato PascalCase (palabras con primera letra
+    // mayúscula, incluida la primera letra)
+    // Ejemplo: GetPlayerController
+
+
+    /// <summary>
+    /// Metodo llamado cuando al transicionar a este estado.
+    /// </summary>
+    public override void EnterState()
+    {
+
+    }
+    
+    /// <summary>
+    /// Metodo llamado antes de cambiar a otro estado.
+    /// </summary>
+    public override void ExitState()
+    {
+        
+    }
+    #endregion
+    
+    // ---- MÉTODOS PRIVADOS O PROTEGIDOS ----
+    #region Métodos Privados o Protegidos
+    // Documentar cada método que aparece aquí
+    // El convenio de nombres de Unity recomienda que estos métodos
+    // se nombren en formato PascalCase (palabras con primera letra
+    // mayúscula, incluida la primera letra)
+
+    /// <summary>
+    /// Metodo llamado cada frame cuando este es el estado activo de la maquina de estados.
+    /// </summary>
+    protected override void UpdateState()
+    {
+        _moveDir = GetCTX<PlayerStateMachine>().PlayerInput.Move.ReadValue<float>();
+    }
+
+    /// <summary>
+    /// Metodo llamado tras UpdateState para mirar si hay que cambiar a otro estado.
+    /// Principalmente es para mantener la logica de cambio de estado separada de la logica del estado en si
+    /// </summary>
+    protected override void CheckSwitchState()
+    { 
+        if (_moveDir != 0)
+        {
+            if (_moveDir < 0)
+            {
+                GetCTX<PlayerStateMachine>().LookingDirection = PlayerStateMachine.PlayerLookingDirection.Left;
+            }
+            else
+            {
+                GetCTX<PlayerStateMachine>().LookingDirection = PlayerStateMachine.PlayerLookingDirection.Right;
+            }
+            ChangeState(Ctx.GetStateByType<PlayerMoveState>());
+        }
+    }
+    #endregion   
+
+} // class PlayerIdleState 
+// namespace
