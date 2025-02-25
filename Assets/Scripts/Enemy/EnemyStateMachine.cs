edfe2831174d8f4dc0422901e268ccbfbe8f2d27
LentaAlexandra <alexlent@ucm.es>
2025-02-25 17:32:59 +0100

    creacion de EnemyStateMachine

--- Assets/Scripts/Enemy/EnemyStateMachine.cs
+++ Assets/Scripts/Enemy/EnemyStateMachine.cs
@@ -8,19 +8,40 @@
 // IMPORTANTE: No uses los métodos del MonoBehaviour: Awake(), Start(), Update, etc. (NINGUNO)
 
 using UnityEngine;
+using UnityEngine.InputSystem.LowLevel;
 // Añadir aquí el resto de directivas using
 
 
 /// <summary>
 /// Máquina de estados del jugador donde se contiene el contexto de todos los estados.
 /// </summary>
+[RequireComponent(typeof(Rigidbody2D))] // Obliga que el GameObject que contenga a este componente tenga un Rigibody2D
+[SelectionBase] // Hace que cuando selecciones el objeto desde el editor se seleccione el que tenga este componente automáticamente
 public class EnemyStateMachine : StateMachine
 {
+    /// <summary>
+    /// <para>
+    /// Codifica las dos formas en las que puede mirar el enemigo.
+    /// </para>
+    /// <remarks> Right = 1, Left = -1 </remarks>
+    /// </summary>
+    public enum EnemyLookingDirection : short
+    {
+        Right = 1,
+        Left = -1,
+    }   
+    
+
     // ---- ATRIBUTOS DEL INSPECTOR ----
     #region Atributos del Inspector (serialized fields)
     // Documentar cada atributo que aparece aquí.
     // Puesto que son atributos globales en la clase debes usar "_" + camelCase para su nombre.
-
+    [Header("Player Information")]
+    /// <summary>
+    /// El Transform del Player. 
+    /// </summary>
+    [Tooltip("The player's transform.")]
+    public Transform _playerTransform;
     #endregion
     
     // ---- ATRIBUTOS PRIVADOS ----
@@ -38,18 +59,43 @@ public class EnemyStateMachine : StateMachine
     #region Propiedades
     // Documentar cada propiedad que aparece aquí.
     // Escribir con PascalCase.
+    /// <summary>
+    /// <para>Dirección en la que mira el enemigo.</para>
+    /// <para>Right = 1, Left = -1.</para>
+    /// Puedes hacer <c>(short)LookingDirection</c> para obtener el valor 1 o -1 directamente.
+    /// </summary>
+    public EnemyLookingDirection LookingDirection { get; set; } = EnemyLookingDirection.Right;
+
+    /// <summary>
+    /// Rigidbody2D del enemigo.
+    /// </summary>
+    public Rigidbody2D Rigidbody { get; private set; }
+
+    /// <summary>
+    /// SpriteRenderer del enemigo.
+    /// </summary>
+    public SpriteRenderer SpriteRenderer { get; private set; }
     #endregion
 
     // ---- MÉTODOS PÚBLICOS ----
     #region Métodos públicos
     // Documentar cada método que aparece aquí con ///<summary>
-    // El convenio de nombres de Unity recomienda que estos métodos
-    // se nombren en formato PascalCase (palabras con primera letra
-    // mayúscula, incluida la primera letra)
-    // Ejemplo: GetPlayerController
+
+    protected override void OnAwake()
+    {
+        Rigidbody = GetComponent<Rigidbody2D>();
+        SpriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    /// <summary>
+    /// Método para notificar de la muerte del enemigo a la máquina de estados.
+    /// </summary>
+    public void Death() {
+        Debug.Log("This enemy has died!");
+    } 
 
     #endregion
-    
+
     // ---- MÉTODOS PRIVADOS O PROTEGIDOS ----
     #region Métodos Privados o Protegidos
     // Documentar cada método que aparece aquí
@@ -57,7 +103,7 @@ public class EnemyStateMachine : StateMachine
     // se nombren en formato PascalCase (palabras con primera letra
     // mayúscula, incluida la primera letra)
 
-    #endregion   
+    #endregion
 
 } // class EnemyStateMachine 
 // namespace
