1d925ac9ccdb8c57724529942de31a73c9b5b330
Alexscratch1 <familiamenen@gmail.com>
2025-03-12 01:03:05 +0100

    HUD_Avanzado

---
+++ Assets/Scripts/Powers/HUD/ChargeBar.cs
@@ -0,0 +1,98 @@
+//---------------------------------------------------------
+// Breve descripción del contenido del archivo
+// Creador del Script: 
+// Kingless Dungeon
+// Proyectos 1 - Curso 2024-25
+//---------------------------------------------------------
+
+using UnityEngine;
+using UnityEngine.UI;
+// Añadir aquí el resto de directivas using
+
+
+/// <summary>
+/// Antes de cada class, descripción de qué es y para qué sirve,
+/// usando todas las líneas que sean necesarias.
+/// </summary>
+public class ChargeBar : MonoBehaviour
+{
+    // ---- ATRIBUTOS DEL INSPECTOR ----
+    #region Atributos del Inspector (serialized fields)
+    // Documentar cada atributo que aparece aquí.
+    // Puesto que son atributos globales en la clase debes usar "_" + camelCase para su nombre.
+    [SerializeField]
+    private float _maxcharge = 100f;
+    [SerializeField] private Slider _ChargeBar;
+    #endregion
+
+    // ---- ATRIBUTOS PRIVADOS ----
+    #region Atributos Privados (private fields)
+    // Documentar cada atributo que aparece aquí.
+    // El convenio de nombres de Unity recomienda que los atributos
+    // privados se nombren en formato _camelCase (comienza con _, 
+    // primera palabra en minúsculas y el resto con la 
+    // primera letra en mayúsculas)
+    // Ejemplo: _maxHealthPoints
+    #endregion
+
+    // ---- PROPIEDADES ----
+    #region Propiedades
+    // Documentar cada propiedad que aparece aquí.
+    // Escribir con PascalCase.
+    private float _charge;
+    #endregion
+
+    // ---- MÉTODOS DE MONOBEHAVIOUR ----
+    #region Métodos de MonoBehaviour
+
+    // Por defecto están los típicos (Update y Start) pero:
+    // - Hay que añadir todos los que sean necesarios
+    // - Hay que borrar los que no se usen 
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before 
+    /// any of the Update methods are called the first time.
+    /// </summary>
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        UpdateChargeBar();
+    }
+    #endregion
+
+    // ---- MÉTODOS PÚBLICOS ----
+    #region Métodos públicos
+    // Documentar cada método que aparece aquí con ///<summary>
+    // El convenio de nombres de Unity recomienda que estos métodos
+    // se nombren en formato PascalCase (palabras con primera letra
+    // mayúscula, incluida la primera letra)
+    // Ejemplo: GetPlayerController
+    public void IncreaseAbilityCharge(int addcharge)
+    {
+        _charge += addcharge;
+    }
+    #endregion
+
+    // ---- MÉTODOS PRIVADOS O PROTEGIDOS ----
+    #region Métodos Privados o Protegidos
+    // Documentar cada método que aparece aquí
+    // El convenio de nombres de Unity recomienda que estos métodos
+    // se nombren en formato PascalCase (palabras con primera letra
+    // mayúscula, incluida la primera letra)
+    private void UpdateChargeBar()
+    {
+        // Controlamos el Slider como un porcentaje (0-1)
+        if (_charge > _maxcharge)
+        {
+            _charge = _maxcharge;
+        }
+        _ChargeBar.value = _charge / _maxcharge;
+
+    }
+    #endregion   
+
+} // class ChargeBar 
+// namespace
