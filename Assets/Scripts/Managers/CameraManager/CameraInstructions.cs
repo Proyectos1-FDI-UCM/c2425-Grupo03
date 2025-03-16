89b6632ac778afa160f2b6820ff6ffae99467a22
Cr00kie <144139282+Cr00kie@users.noreply.github.com>
2025-03-16 15:29:50 +0100

    Añade documentación y actualiza algunos prefabs desactualizados

--- Assets/Scripts/Managers/CameraManager/CameraInstructions.cs
+++ Assets/Scripts/Managers/CameraManager/CameraInstructions.cs
@@ -16,18 +16,25 @@ public abstract class CameraInstruction
 
     // ---- ATRIBUTOS PRIVADOS ----
     #region Atributos Privados (private fields)
+    /// <summary>
+    /// El siguiente valor de zoom deseado.
+    /// </summary>
     protected float _nextZoom;
+    /// <summary>
+    /// La velocidad a la que se debe aumentar el zoom por segundo para llegar al zoom deseado al final de la instrucción.
+    /// </summary>
     protected float _zoomSpeed;
+    /// <summary>
+    /// El momento desde el comienzo del programa cuando termina la instrucción. (Esta en segundos)
+    /// </summary>
     protected float _endTime;
+    /// <summary>
+    /// Cuanto dura la instrucción en segundos.
+    /// </summary>
     private float _actionLength;
 
     #endregion
 
-    // ---- PROPIEDADES ----
-    #region Propiedades
-    // Documentar cada propiedad que aparece aquí.
-    // Escribir con PascalCase.
-    #endregion
 
 
     // ---- MÉTODOS PÚBLICOS ----
@@ -58,23 +65,16 @@ public abstract class CameraInstruction
         throw new UnityException("Not implemented camera method");
     }
 
-    public virtual void OnDequeued()
-    {
-
-    }
+    /// <summary>
+    /// Método llamado cuando se quita de la queue a la instrucción.
+    /// </summary>
+    public virtual void OnDequeued() { }
     #endregion
-    
-    // ---- MÉTODOS PRIVADOS O PROTEGIDOS ----
-    #region Métodos Privados o Protegidos
-    // Documentar cada método que aparece aquí
-    // El convenio de nombres de Unity recomienda que estos métodos
-    // se nombren en formato PascalCase (palabras con primera letra
-    // mayúscula, incluida la primera letra)
-
-    #endregion   
-
 } // class CameraInstructions 
 
+/// <summary>
+/// Instrucción de la cámara que permite mover la cámara hacia una posición.
+/// </summary>
 public sealed class CameraPan : CameraInstruction
 {
     // ---- ATRIBUTOS PRIVADOS
@@ -92,6 +92,12 @@ public sealed class CameraPan : CameraInstruction
 
     // ---- MÉTODOS PUBLICOS
     #region Métodos Públicos
+    /// <summary>
+    /// Constructor de la instucción CameraPan.
+    /// </summary>
+    /// <param name="target">El objetivo hacia el que mover la cámara.</param>
+    /// <param name="actionLength">La duración deseada del paneo.</param>
+    /// <param name="nextZoom">El zoom deseado al final del paneo.</param>
     public CameraPan(Vector3 target, float actionLength, float nextZoom) : base(actionLength, nextZoom)
     {
         _target = target;
@@ -100,12 +106,15 @@ public sealed class CameraPan : CameraInstruction
 
     public override bool UpdateCamera(Camera camera)
     {
+        //Si hemos llegado al objetivo termina la instrucción
         if (camera.transform.position.sqrMagnitude == _target.sqrMagnitude)
         {
             return true;
         }
 
+        //Movemos la cámara hacia el objetivo
         camera.transform.position = Vector3.MoveTowards(camera.transform.position, _target, _cameraSpeed * Time.deltaTime);
+        //Actualizamos el zoom
         camera.orthographicSize = Mathf.MoveTowards(camera.orthographicSize, _nextZoom, _zoomSpeed * Time.deltaTime);
 
         return false;
@@ -113,22 +122,29 @@ public sealed class CameraPan : CameraInstruction
     #endregion
 }
 
+/// <summary>
+/// Instrucción de la cámara que permite hacer a la cámara esperar un tiempo determinado.
+/// </summary>
 public sealed class CameraWait : CameraInstruction
 {
     // ---- MÉTODOS PUBLICOS
     #region Métodos Públicos
-    public CameraWait(float actionLength, float nextZoom) : base(actionLength, nextZoom)
-    {
-
-    }
+    /// <summary>
+    /// Constructor de la instucción CameraWait.
+    /// </summary>
+    /// <param name="actionLength">Duración de espera hasta la siguiente instrucción.</param>
+    /// <param name="nextZoom">Zoom deseado al final de la espera.</param>
+    public CameraWait(float actionLength, float nextZoom) : base(actionLength, nextZoom) { }
 
     public override bool UpdateCamera(Camera camera)
     {
+        // Si el zoom ha sido alcanzado y ha terminado el tiempo de espera termina la instrucción
         if(camera.orthographicSize == _nextZoom && Time.time > _endTime)
         {
             return true;
         }
 
+        // Actualizamos el zoom
         camera.orthographicSize = Mathf.MoveTowards(camera.orthographicSize, _nextZoom, _zoomSpeed * Time.deltaTime);
 
         return false;
@@ -136,35 +152,42 @@ public sealed class CameraWait : CameraInstruction
     #endregion
 }
 
+/// <summary>
+/// Instrucción de la cámara que permite hacer que la cámara siga al jugador un cierto tiempo. <b>(Si no hay más instrucciones lo sigue siguiendo)</b>
+/// </summary>
 public sealed class CameraFollowPlayer : CameraInstruction
 {
-    // ---- ATRIBUTOS PRIVADOS
-    #region Atributos Privados
-    #endregion region
 
     // ---- MÉTODOS PUBLICOS
     #region Métodos Públicos
-    public CameraFollowPlayer(float actionLength, float nextZoom) : base(actionLength, nextZoom)
-    {
-    }
+    /// <summary>
+    /// Constructor de la instrucción CameraFollowPlayer
+    /// </summary>
+    /// <param name="actionLength">Durante cuanto tiempo debe seguir al jugador.</param>
+    /// <param name="nextZoom">Zoom deseado al final de la instrucción.</param>
+    public CameraFollowPlayer(float actionLength, float nextZoom) : base(actionLength, nextZoom) { }
 
     public override bool UpdateCamera(Camera camera)
     {
+        // Cuando se ha alcanzado el tiempo indicado y el zoom indicado termina la acción
         if (camera.orthographicSize == _nextZoom && Time.time > _endTime)
         {
             return true;
         }
         else
         {
+            // Se le indica al manager que siga al jugador
             CameraManager.Instance.IsFollowingPlayer = true;
+            // Se actualiza el zoom
             camera.orthographicSize = Mathf.MoveTowards(camera.orthographicSize, _nextZoom, _zoomSpeed * Time.deltaTime);
-            Debug.Log(camera.orthographicSize);
+
             return false;
         }
     }
 
     public override void OnDequeued()
     {
+        // Cuando comienze una nueva instrucción le avisamos al manager que deje de seguir al jugador.
         CameraManager.Instance.IsFollowingPlayer = false;
     }
     #endregion
--- Assets/Scripts/Managers/CameraManager/CameraManager.cs
+++ Assets/Scripts/Managers/CameraManager/CameraManager.cs
@@ -1,108 +1,172 @@
 //---------------------------------------------------------
-// Breve descripción del contenido del archivo;
-// Script del movimiento de cámara para que siga al jugador
-// Responsable: SANTIAGO SALTO
+// Archivo que maneja la camara y las instrucciones que se le dan.
+// Adrián Isasi
 // Kingless Dungeon
 // Proyectos 1 - Curso 2024-25
 //---------------------------------------------------------
-/*
+
+using System.Collections.Generic;
 using UnityEngine;
 
 
 /// <summary>
-/// Componente responsable de la gestión de la cámara del juego
+/// Manager de la cámara. Maneja el movimiento e instrucciones de la cámara.
 /// </summary>
-public class CameraManagerObs : MonoBehaviour
+public class CameraManager : MonoBehaviour
 {
     // ---- ATRIBUTOS DEL INSPECTOR ----
     #region Atributos del Inspector (serialized fields)
-    // Documentar cada atributo que aparece aquí.
-    // Puesto que son atributos globales en la clase debes usar "_" + camelCase para su nombre.
-  
     /// <summary>
     /// Posicion Jugador u objeto.
     /// </summary>
     [SerializeField] Transform _playerPosition;
+
     /// <summary>
     /// velocidad de la Cámara
     /// </summary>
     [SerializeField][Min(0)] float _cameraVelocity;
+
     /// <summary>
     /// Margen de la Cámara respecto al objetivo
     /// </summary>
-    [SerializeField] private Vector3 _cameraDisplacement; //dista de la camara (posicion del jugador)
+    [SerializeField] private Vector3 _cameraDisplacement;
+
+    /// <summary>
+    /// Desplazamiento máximo de la cámara desde el jugador (al moverla con las teclas o joystick)
+    /// </summary>
     [SerializeField] private float _maxDisplacement;
     #endregion
-    
+
     // ---- ATRIBUTOS PRIVADOS ----
     #region Atributos Privados (private fields)
     /// <summary>
+    /// Las instrucciones a ejecutar de la cámara.
+    /// </summary>
+    Queue<CameraInstruction> _cameraInstructions = new Queue<CameraInstruction>();
+
+    /// <summary>
+    /// La cámara a actualizar.
+    /// </summary>
+    Camera _camera;
+
+    /// <summary>
     /// Dirección de movimiento de la cámara.
     /// </summary>
-    private Vector3 _moveDir; 
+    private Vector3 _moveDir;
+
+    /// <summary>
+    /// El input del jugador.
+    /// </summary>
     private PlayerInputActions.PlayerActions _playerInput;
-    private Vector3 _finalPos;
-    
     #endregion
 
     // ---- PROPIEDADES ----
     #region Propiedades
-    // Documentar cada propiedad que aparece aquí.
-    // Escribir con PascalCase.
-    #endregion 
-    
+    /// <summary>
+    /// La instancia del singleton CameraManager.
+    /// </summary>
+    public static CameraManager Instance { get; private set; }
+
+    /// <summary>
+    /// Getter de la cámara para tener acceso global.
+    /// </summary>
+    public Camera Camera => _camera;
+
+    /// <summary>
+    /// Booleana que le indica al manager si debe seguir al jugador o no.
+    /// </summary>
+    public bool IsFollowingPlayer { get; set; } = false;
+
+    #endregion
+
     // ---- MÉTODOS DE MONOBEHAVIOUR ----
     #region Métodos de MonoBehaviour
-    
-    void Awake() {
-        _playerInput = new PlayerInputActions().Player;
-        _playerInput.Enable();
+
+    void Awake()
+    {
+        // Patrón singleton
+        if(Instance == null)
+        {
+            Instance = this;
+
+            // Establece la variable de cámara
+            _camera = GetComponent<Camera>();
+
+            // Coge el input del jugador
+            _playerInput = new PlayerInputActions().Player;
+            _playerInput.Enable();
+
+            //Pone la instrucción de seguir al jugador por defecto
+            EnqueueInstruction(new CameraFollowPlayer(1, 7));
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
+    /// <summary>
+    /// Actualiza la instrucción actual y cambia de instrucción cuando esta termina.
+    /// </summary>
     void Update()
     {
+        if (_cameraInstructions.Count > 0)
+        {
+            // Actualiza la camara con la instrucción actual.
+            bool instructionEnded = _cameraInstructions.Peek().UpdateCamera(_camera);
+
+            // Si la instrucción ha terminado y hay más instrucciones, pasa a la siguiente instrucción.
+            if (instructionEnded && _cameraInstructions.Count > 1)
+            {
+                // Avisa que va a cambiar de instrucción
+                _cameraInstructions.Peek().OnDequeued();
+
+                // Cambia la instrucción
+                _cameraInstructions.Dequeue();
+
+                // Avisa de que ha comenzado la instrucción
+                _cameraInstructions.Peek().SetUp();
+            }
+
+            /* PARA TESTEAR
+            string str = "Camera Queue: ";
+            foreach(CameraInstruction instruction in _cameraInstructions)
+            {
+                str += instruction.ToString() + ", ";
+            }
+            Debug.Log(str);
+            */
+        }
+
         // Lee el input del jugador.
         _moveDir = _playerInput.MoveCamera.ReadValue<Vector2>();
-       
     }
 
     void FixedUpdate()
     {
-        if (_playerPosition != null) 
+        // Si la cámara está en la instrucción de seguir al jugador la mueve hacia el jugador y aplica el desplazamiento dado por el jugador
+        if (IsFollowingPlayer && _playerPosition != null)
         {
-             _finalPos = _playerPosition.position + _moveDir * _maxDisplacement + _cameraDisplacement; // Calcula la posición final (posición del jugador + el input * el desplazamiento máximo posible)
-            Move(_finalPos); // la cámara sigue al jugador
+            Vector3 finalPos = _playerPosition.position + _moveDir * _maxDisplacement + _cameraDisplacement; // Calcula la posición final (posición del jugador + el input * el desplazamiento máximo posible)
+            transform.position = Vector3.Lerp(transform.position, finalPos, _cameraVelocity);
         }
     }
 
- 
     #endregion
 
     // ---- MÉTODOS PÚBLICOS ----
     #region Métodos públicos
-    // Documentar cada método que aparece aquí con ///<summary>
-    // El convenio de nombres de Unity recomienda que estos métodos
-    // se nombren en formato PascalCase (palabras con primera letra
-    // mayúscula, incluida la primera letra)
-    // Ejemplo: GetPlayerController
+    public void EnqueueInstruction(CameraInstruction cameraInstruction)
+    {
+        // pone la instrucción a la cola
+        _cameraInstructions.Enqueue(cameraInstruction);
+
+        // si es la primera instrucción la inicializa
+        if(_cameraInstructions.Count == 0) cameraInstruction.SetUp();
+    }
 
     #endregion
     
-    // ---- MÉTODOS PRIVADOS O PROTEGIDOS ----
-    #region Métodos Privados o Protegidos
-    /// <summary>
-    /// Método para mover la cámara a la posición indicada.
-    /// </summary>
-    /// <param name="movePos">La posición a la que mover la cámara.</param>
-    private void Move(Vector3 movePos) {
-        //Hacer que el movimiento se vea gradual
-        Vector3 smoothedMovement = Vector3.Lerp(transform.position, movePos, _cameraVelocity);
-        //Mover la Cámara
-        transform.position = smoothedMovement;
-    }
 
-    #endregion   
-
-} // class CameraManager 
+} // class Camera 
 // namespace
-*/
