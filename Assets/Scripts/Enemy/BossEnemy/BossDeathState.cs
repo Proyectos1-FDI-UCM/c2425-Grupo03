e079e785eb53e18bd7173b6973bc844484cfecd2
Adrián Isasi <144139282+Cr00kie@users.noreply.github.com>
2025-05-03 17:50:50 +0200

    Añade el estado vulnerable de la segunda fase del boss y el estado de muerte

---
+++ Assets/Scripts/Enemy/BossEnemy/BossDeathState.cs
@@ -0,0 +1,106 @@
+//---------------------------------------------------------
+// Estado de muerte del boss
+// Adrián Isasi
+// Kingless Dungeon
+// Proyectos 1 - Curso 2024-25
+//---------------------------------------------------------
+
+using UnityEngine;
+// Añadir aquí el resto de directivas using
+
+
+/// <summary>
+/// Antes de cada class, descripción de qué es y para qué sirve,
+/// usando todas las líneas que sean necesarias.
+/// </summary>
+public class BossDeathState : BaseState
+{
+    // ---- ATRIBUTOS DEL INSPECTOR ----
+    #region Atributos del Inspector (serialized fields)
+    [SerializeField]
+    float _timeToDestroy;
+    #endregion
+    
+    // ---- ATRIBUTOS PRIVADOS ----
+    #region Atributos Privados (private fields)
+    // Documentar cada atributo que aparece aquí.
+    // El convenio de nombres de Unity recomienda que los atributos
+    // privados se nombren en formato _camelCase (comienza con _, 
+    // primera palabra en minúsculas y el resto con la 
+    // primera letra en mayúsculas)
+    // Ejemplo: _maxHealthPoints
+
+    #endregion
+
+    // ---- PROPIEDADES ----
+    #region Propiedades
+    // Documentar cada propiedad que aparece aquí.
+    // Escribir con PascalCase.
+    #endregion
+    
+    // ---- MÉTODOS DE MONOBEHAVIOUR ----
+    #region Métodos de MonoBehaviour
+    
+    #endregion
+
+    // ---- MÉTODOS PÚBLICOS ----
+    #region Métodos públicos
+    // Documentar cada método que aparece aquí con ///<summary>
+    // El convenio de nombres de Unity recomienda que estos métodos
+    // se nombren en formato PascalCase (palabras con primera letra
+    // mayúscula, incluida la primera letra)
+    // Ejemplo: GetPlayerController
+
+    
+    /// <summary>
+    /// Metodo llamado cuando al transicionar a este estado.
+    /// </summary>
+    public override void EnterState()
+    {
+        if(Ctx.TryGetComponent(out HealthManager hm))
+        {
+            hm.Inmune = true;
+        }
+
+        Destroy(transform.root.gameObject, _timeToDestroy);
+    }
+    
+    /// <summary>
+    /// Metodo llamado antes de cambiar a otro estado.
+    /// </summary>
+    public override void ExitState()
+    {
+        
+    }
+    #endregion
+    
+    // ---- MÉTODOS PRIVADOS O PROTEGIDOS ----
+    #region Métodos Privados o Protegidos
+
+
+    // Documentar cada método que aparece aquí
+    // El convenio de nombres de Unity recomienda que estos métodos
+    // se nombren en formato PascalCase (palabras con primera letra
+    // mayúscula, incluida la primera letra)
+
+    /// <summary>
+    /// Metodo llamado cada frame cuando este es el estado activo de la maquina de estados.
+    /// </summary>
+    protected override void UpdateState()
+    {
+        
+    }
+
+    /// <summary>
+    /// Metodo llamado tras UpdateState para mirar si hay que cambiar a otro estado.
+    /// Principalmente es para mantener la logica de cambio de estado separada de la logica del estado en si
+    /// </summary>
+    protected override void CheckSwitchState()
+    {
+        
+    }
+
+    #endregion   
+
+} // class BossDeathState 
+// namespace
